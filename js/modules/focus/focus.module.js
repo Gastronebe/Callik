035929fb0c4bd407293a3363c68cf65c8d03ef0e
Pavlína Pavlusha Šteiglová <info@callik.fun>
2026-01-19 22:15:10 +0100

    Focus: vytvořen FocusModule

---
+++ js/modules/focus/focus.module.js
@@ -0,0 +1,13 @@
+// Focus modul - stará se o focus timer a kruhový ukazatel
+
+export class FocusModule {
+    constructor() {
+      // sem později přesuneme stav (např. interval, zbývající čas)
+    }
+  
+    async init() {
+      console.log('⏱️ Inicializuji Focus modul...');
+      // sem později dáme napojení na DOM a start timeru
+    }
+  }
+  
